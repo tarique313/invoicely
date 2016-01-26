48a70d0152d0b9c1ed46de72684cbed79dfde96f
tarique313 <mahmud.tarique@gmail.com>
2016-01-27 01:30:09 +0600

    Finish download csv files

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,4 +6,14 @@ def self.import(file)
 	end
  end
 
+ def self.to_csv 
+CSV.generate do |csv|
+
+	csv << column_names
+	all.each do |company|
+		csv << company.attributes.values_at(*column_names)
+ 	end
+ end
+end
+
 end
